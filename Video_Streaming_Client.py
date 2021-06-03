23abe8cfe222815ca69fb9a62ae82c1acdc6e11a
Anurag Hansraj Yadav <69779873+yanuragaj@users.noreply.github.com>
2021-06-03 18:04:24 +0530

    Add files via upload

---
+++ Video_Streaming_Client.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2, socket, pickle, os
+
+
+# In[2]:
+
+
+s=socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
+serverip="192.168.43.54"
+serverport=307
+
+
+# In[31]:
+
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret,photo = cap.read()
+    cv2.imshow('my pic', photo)
+    ret, buffer = cv2.imencode(".jpg", photo)
+    x_as_bytes = pickle.dumps(buffer)
+    s.sendto(x_as_bytes,(serverip , serverport))
+    if cv2.waitKey(10) == 13:
+        break
+  
+cv2.destroyAllWindows()
+cap.release()
+
+
+# In[ ]:
+
+
+
+
---
+++ Video_Streaming_Server.py
@@ -0,0 +1,20 @@
+import cv2, socket, numpy, pickle
+s=socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+ip="192.168.43.54"
+port=307
+s.bind((ip,port))
+while True:
+    x=s.recvfrom(100000000)
+    clientip = x[1][0]
+    data=x[0]
+    print(data)
+    data=pickle.loads(data)
+    print(type(data))
+    data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    cv2.imshow('my pic', data) #to open image
+    if cv2.waitKey(10) == 13:
+        break
+cv2.destroyAllWindows()
+
+
+
